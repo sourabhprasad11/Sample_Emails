0f80d9a221adaaf79836038c65fd0f4da18abbdf
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-12 18:52:36 +0530

    Valid Emails code added

--- ValidEmails.sh
+++ ValidEmails.sh
@@ -2,3 +2,11 @@
 
 echo "Valid Emails"
 
+patt="^([^0-9][a-zA-Z0-9\_\-\.]+)@([a-zA-Z]+)\.([a-z]{2,4})\.([a-z]{2,4})$"
+read -p "Enter Email(abc.xyz@bl.co.in): " z
+
+if [[ $z =~ $patt ]]; then
+   echo "Valid Email"
+else
+   echo "Invalid Email"
+fi
